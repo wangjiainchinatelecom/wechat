ce54476188bff87035b2d1c94cbe9d7c15f372b9
wangjia <wangjia@ctyun.cn>
2016-11-27 09:16:04 +0800

    返回结果用httpResponse

--- csd/views.py
+++ csd/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
 
 # Create your views here.
 @api_view(["GET"])
@@ -12,4 +13,4 @@ def checkSignature(request):
     timestamp = params["timestamp"]
     echostr = params["echostr"]
     result={"echostr":echostr}
-    return Response(result)
+    return HttpResponse(echostr)
