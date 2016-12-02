c26017e117a07dbe847af9dfd181f59849c3db14
wangjia <wangjia@ctyun.cn>
2016-12-02 22:40:47 +0800

    转发消息到客服

--- csd/views.py
+++ csd/views.py
@@ -4,22 +4,36 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
 from wechat import api
+import datetime,time
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET","POST"])
 def checkSignature(request):
     params = request.query_params
-    nonce = params["nonce"]
-    signature = params["signature"]
-    timestamp = params["timestamp"]
-    echostr = params["echostr"]
-    if api.checkSignature(signature,timestamp,nonce):
-        result=echostr
-    else:
-        result='request is not allow!'
+    nonce = params.get("nonce")
+    signature = params.get("signature")
+    timestamp = params.get("timestamp")
+    echostr = params.get("echostr")
+    touser = params.get("openid")
+    if request.method == "GET":
+        if api.checkSignature(signature,timestamp,nonce):
+            result=echostr
+        else:
+            result='request is not allow!'
+    if request.method == "POST":
+        create_time = datetime.datetime.now()
+        result = "<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime>{}</CreateTime><MsgType><![CDATA[transfer_customer_service]]></MsgType></xml>".format(touser,"chenlong828_",time.mktime(create_time))
     return HttpResponse(result)
 
 @api_view(["GET"])
 def getAccessToken(request):
     result = api.get_access_token()
     return HttpResponse(result)
+
+@api_view(["GET","POST"])
+def printLog(request):
+    if request.http_method_names =="GET":
+        print request.query_params
+    elif request.http_method_names =="POST":
+        print request.data
+    return HttpResponse("success")
