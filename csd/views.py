283ca6e6a80c94486b423037cf780c40e025612c
wangjia <85054368@qq.com>
2016-12-06 15:15:50 +0800

    去掉@api_view头，因为微信发来的是xml，不是json，会转换出错

--- csd/views.py
+++ csd/views.py
@@ -4,10 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
 from wechat import api
-import datetime,time
+import datetime,time,xmltodict
 
 # Create your views here.
-@api_view(["GET","POST"])
 def checkSignature(request):
     params = request.query_params
     nonce = params.get("nonce")
@@ -21,16 +20,24 @@ def checkSignature(request):
         else:
             result='request is not allow!'
     if request.method == "POST":
+        xml_demo = "<xml><ToUserName><![CDATA[gh_3b94bedd4725]]></ToUserName>" \
+                   "<FromUserName><![CDATA[oBkphwBVZDhsqGPrGEl-6jopQlCw]]></FromUserName>" \
+                   "<CreateTime>1480988559</CreateTime>"\
+                   "<MsgType><![CDATA[text]]></MsgType>"\
+                   "<Content><![CDATA[3]]></Content>"\
+                   "<MsgId>6360797427111793535</MsgId>"\
+                   "</xml>"
+        xml = request.body
         create_time = datetime.datetime.now().timetuple()
         result = "<xml><ToUserName><![CDATA[{}]]></ToUserName><FromUserName><![CDATA[{}]]></FromUserName><CreateTime>{}</CreateTime><MsgType><![CDATA[transfer_customer_service]]></MsgType></xml>".format(touser,"wj_851026",time.mktime(create_time))
     return HttpResponse(result)
 
-@api_view(["GET"])
+
 def getAccessToken(request):
     result = api.get_access_token()
     return HttpResponse(result)
 
-@api_view(["GET","POST"])
+
 def printLog(request):
     if request.http_method_names =="GET":
         print request.query_params
--- wechat/constants.py
+++ wechat/constants.py
@@ -1,3 +1,3 @@
 tonken=u'testwechat'
-APPID=u'wx01efd1c3d8bea011'
-APPSECRET=u'6f0f64a97c5468c68de16ec72f2e7210'
+APPID=u'wxa6b67c51aa49d591'
+APPSECRET=u'59ff2210cf83ada74d1492b292e18fcf'
